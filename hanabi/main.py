640c578e9713112926e01918b69ffeed03da6142
lekha <305097+lekha@users.noreply.github.com>
2017-11-19 10:15:25 -0500

    FIX TYPOS - SQUASH

--- hanabi/main.py
+++ hanabi/main.py
@@ -7,12 +7,12 @@ COLORS = {'blue', 'green', 'red', 'white', 'yellow'}
 
 
 def new_deck():
-    deck = set()
+    deck = []
     for color in COLORS:
         for rank in [1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]:
-            deck.add(Card(color, rank))
+            deck.append(Card(color, rank))
     return deck
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     deck = new_deck()
