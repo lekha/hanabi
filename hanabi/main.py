395f4c9b3dc05d83264ebfaf84e84d752dcc9a66
lekha <305097+lekha@users.noreply.github.com>
2017-12-13 15:14:37 -0500

    Implement DrunkPlayer
    Game can be played but all cards are always discarded.

--- hanabi/main.py
+++ hanabi/main.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from random import shuffle
 
-from hanabi.players import Player
+from hanabi.players import DrunkPlayer
 
 
 Card = namedtuple('Card', ['color', 'rank'])
@@ -29,15 +29,33 @@ def new_deck():
 def new_game():
     remaining_clue_tokens = STARTING_CLUE_TOKENS
     remaining_fuse_tokens = STARTING_FUSE_TOKENS
+    discard_stack = []
+    firework_stacks = {color: [] for color in COLORS}
     deck = new_deck()
 
     num_players = 3
     cards_per_hand = NUM_PLAYERS_TO_NUM_CARDS_PER_HAND[num_players]
     players = []
     for _ in range(num_players):
-        players.append(Player(cards_per_hand))
+        players.append(DrunkPlayer(cards_per_hand))
         for _ in range(cards_per_hand):
             players[-1].receive_cards(deck.pop())
+
+    def game_over():
+        return not deck or remaining_fuse_tokens == 0
+
+    shuffle(players)
+    current_player_index = 0
+    while not game_over():
+        current_player = players[current_player_index]
+        action = current_player.choose_action()
+        if action == 'discard':
+            card = current_player.discard_card()
+
+        if not current_player.has_enough_cards():
+            current_player.receive_cards(deck.pop())
+
+        current_player_index = (current_player_index+1) % num_players
 
 
 if __name__ == '__main__':
--- hanabi/players.py
+++ hanabi/players.py
@@ -1,20 +1,25 @@
 """Hanabi player interface."""
 from hanabi.errors import TooManyCardsInHandError
 
-
 class Player(object):
     def __init__(self, card_limit):
         self.cards = []
         self.limit = card_limit
 
+    def has_enough_cards(self):
+        return self.cards == self.limit
+
+    def choose_action(self):
+        raise NotImplementedError
+
     def play_card(self):
-        pass
+        raise NotImplementedError
 
     def discard_card(self):
-        pass
+        raise NotImplementedError
 
     def give_clue(self):
-        pass
+        raise NotImplementedError
 
     def receive_cards(self, *cards):
         if len(self.cards)+len(cards) <= self.limit:
@@ -22,3 +27,13 @@ class Player(object):
         else:
             raise TooManyCardsInHandError(
                 "Maximum of %s cards allowed in hand" % self.limit)
+
+
+class DrunkPlayer(Player):
+    def choose_action(self):
+        return 'discard'
+
+    def discard_card(self):
+        assert len(self.cards) != 0
+        discarded_card = self.cards.pop(0)
+        return discarded_card
